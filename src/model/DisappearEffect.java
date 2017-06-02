27a8449db42a7239eb00f6a1008cd32c152b6e00
dicoju <julien.culieras@gmail.com>
2017-06-02 17:07:25 +0200

    Commit Q4
    ajout de l’effet qui fait disparaître un jeton

--- src/model/DisappearEffect.java
+++ src/model/DisappearEffect.java
@@ -15,7 +15,17 @@ public class DisappearEffect extends Effect {
      */
     @Override
     public void playEffect(int line, int column, Game game) {
+        int tile_id = game.getBoard().getTileIJ(line, column).getStatus();
+        int player1_id = game.getPlayer1().getId();
+        int player2_id = game.getPlayer2().getId();
 
+        if (tile_id == player1_id){
+            tile_id = -1;
+        }
+        else if(tile_id == player2_id){
+            tile_id = -1;
+        }
+        game.getBoard().getTileIJ(line, column).setStatus(tile_id);
     }
 
 }
