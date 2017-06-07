8bca90b98bf9849ef393254703c703af45033b59
dicoju <julien.culieras@gmail.com>
2017-06-07 10:41:11 +0200

    ajout effet disparition colone

--- src/model/EffectFactory.java
+++ src/model/EffectFactory.java
@@ -22,9 +22,21 @@ public class EffectFactory {
     public static Effect createEffect() {
         int min = 0;
         int max = 2;
-        return new ChangeColorEffect();
-
+        Random rand = new Random();
+        //Tire un nombre aléatoire entre min et max compris
+        int random = rand.nextInt(max - min + 1) + min;
         
+        switch (random) {
+
+            case 0:
+                return new ChangeColorEffect();
+            case 1:
+                return new DisappearEffect();
+            case 2:
+                return new DisparitionColoneEffet();
+        }
+
+        return null;
 
     }
     
