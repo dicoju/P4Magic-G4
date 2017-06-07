5ef2b96d916f430b78429cfd91beea5acb15a7dc
dicoju <julien.culieras@gmail.com>
2017-06-07 10:28:11 +0200

    test commit pour charger effet

--- src/model/DisparitionColoneEffet.java
+++ src/model/DisparitionColoneEffet.java
@@ -9,6 +9,9 @@ public class DisparitionColoneEffet extends Effect {
     @Override
     public void playEffect(int line, int column, Game game) {
         
+        int i;
+        for(i = 0; i < 10; i++){
+            game.getBoard().getTileIJ(i, column).setStatus(-1);
+        }
     }
-    
 }
--- src/model/EffectFactory.java
+++ src/model/EffectFactory.java
@@ -21,20 +21,10 @@ public class EffectFactory {
      */
     public static Effect createEffect() {
         int min = 0;
-        int max = 1;
-        Random rand = new Random();
-        //Tire un nombre aléatoire entre min et max compris
-        int random = rand.nextInt(max - min + 1) + min;
+        int max = 2;
+        return new ChangeColorEffect();
+
         
-        switch (random) {
-
-            case 0:
-                return new ChangeColorEffect();
-            case 1:
-                return new DisappearEffect();
-        }
-
-        return null;
 
     }
     
