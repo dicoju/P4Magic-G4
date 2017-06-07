9db0c4ca624cda97612c03ddc445f752c6f6c7cd
dicoju <julien.culieras@gmail.com>
2017-06-07 10:09:09 +0200

    resolution bug
    bug conflit

--- src/model/DisparitionColoneEffet.java
+++ src/model/DisparitionColoneEffet.java
@@ -8,7 +8,7 @@ public class DisparitionColoneEffet extends Effect {
 
     @Override
     public void playEffect(int line, int column, Game game) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
